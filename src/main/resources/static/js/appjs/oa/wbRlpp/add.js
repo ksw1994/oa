048b04d0518ce12d7401a821a7dd6e0e02e20c44
xm <xm@163.com>
2020-01-10 10:43:46 +0800

    add

--- src/main/resources/static/js/appjs/oa/wbRlpp/add.js
+++ src/main/resources/static/js/appjs/oa/wbRlpp/add.js
@@ -3,35 +3,53 @@ $(function(){
 			curTime = new Date();
 	var curDate = curTime.getFullYear() + '-' + (curTime.getMonth()+1+'').padStart(2,'0');
 
-	function getNewDate(date,n){
-		if(!n) n = 1;
+	function getNewDate(sp,n,date){
+		if(arguments.length === 1 && typeof arguments[0] === 'number') {
+			n = sp;
+			sp = '-';
+		}
+		if(!n) n = -1;
+		if(!date) date = curDate;
 		var d = new Date(date);
 		var dm = d.getMonth() + n;
 		d.setMonth(dm);
 		d_year = d.getFullYear();
 		d_month = (d.getMonth()+1+'').padStart(2,'0');
-		return (d_year+'-'+d_month);
+		return (d_year + sp + d_month);
 	}
 
 	function rendTBody(){
 		$('#table1 tbody').html('<tr>'+
-									'<td style="text-align:center;">'+getNewDate(curDate,-2)+'</td>'+
-									'<td style="text-align:center;"><input name="items1" class="form-control" type="text"></td>'+
-									'<td></td>'+
-								'</tr>'+
-								'<tr>'+
-									'<td style="text-align:center;">'+getNewDate(curDate,-1)+'</td>'+
-									'<td style="text-align:center;"><input name="items2" class="form-control" type="text"></td>'+
-									'<td></td>'+
-								'</tr>'+
-								'<tr>'+
-									'<td style="text-align:center;">'+curDate+'</td>'+
-									'<td style="text-align:center;"><input name="items3" class="form-control" type="text"></td>'+
-									'<td></td>'+
-								'</tr>');
+			'<td style="text-align:center;">'+curDate+'</td>'+
+			'<td style="text-align:center;"><input name="items1-'+curDate.replace('-','')+'" class="form-control" type="text"></td>'+
+			'<td></td>'+
+		'</tr>'+
+		'<tr>'+
+			'<td style="text-align:center;">'+getNewDate(-1)+'</td>'+
+			'<td style="text-align:center;"><input name="items2-'+getNewDate('',-1)+'" class="form-control" type="text"></td>'+
+			'<td></td>'+
+		'</tr>'+
+		'<tr>'+
+			'<td style="text-align:center;">'+getNewDate(-2)+'</td>'+
+			'<td style="text-align:center;"><input name="items3-'+getNewDate('',-2)+'" class="form-control" type="text"></td>'+
+			'<td></td>'+
+		'</tr>');
+		curDate = getNewDate(-2);
 	}
 
 	function init(){
+		$.ajax({
+			type : "get",
+			url : "/oa/project/getAll",
+			error : function(request) {
+				parent.layer.alert("Connection error");
+			},
+			success : function(data) {
+				//if (data.code == 0) {
+					console.log(data);
+			}
+		});
+
 		$('#endDate').val(curDate);
 		rendTBody();
 	}
@@ -45,11 +63,39 @@ $(function(){
 	});
 
 	$('.addmore a').click(function(){
-		curDate = getNewDate(curDate);
+		curDate = getNewDate(-1);
 		items++;
-		$('#table1').append('<tr><td style="text-align:center;">'+ curDate +'</td><td style="text-align:center;"><input class="form-control" name="item'+items+'" type="text"></td><td><a class="btn btn-warning btn-sm" href="#" title="删除" onclick="removeTR(this)"><i class="fa fa-remove"></i></a></td></tr>');
+		$('#table1').append('<tr><td style="text-align:center;">'+ curDate +'</td><td style="text-align:center;"><input class="form-control" name="items'+items+'-'+curDate.replace('-','')+'" type="text"></td><td><a class="btn btn-warning btn-sm" href="#" title="删除" onclick="removeTR(this)"><i class="fa fa-remove"></i></a></td></tr>');
 	})
 
+	$('#signupForm').submit(function(e){
+		e.preventDefault();
+		save();
+		return;
+	})
+
+	function save() {
+		$.ajax({
+			cache : true,
+			type : "POST",
+			url : "",
+			data : $('#signupForm').serialize(),// 你的formid
+			async : false,
+			error : function(request) {
+				parent.layer.alert("Connection error");
+			},
+			success : function(data) {
+				if (data.code == 0) {
+					parent.layer.msg("操作成功");
+					parent.reLoad();
+					var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
+					parent.layer.close(index);
+				} else {
+					parent.layer.alert(data.msg)
+				}
+			}
+		});
+	}
 })
 function removeTR(t){
 	$(t).parent().parent().remove();
