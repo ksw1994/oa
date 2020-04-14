22200463ae03cdd05bc51eca528f419c597595ff
keshengwei <997318565@qq.com>
2020-04-14 14:36:40 +0800

    启动

--- src/main/java/com/bootdo/BootdoApplication.java
+++ src/main/java/com/bootdo/BootdoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAutoConfiguration(exclude = {
         org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
